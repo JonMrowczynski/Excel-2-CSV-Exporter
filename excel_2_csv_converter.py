367843b3dabef0c98c21d8af93572b0a415d75cd
Jon Mrowczynski <jonmrowczynski@gmail.com>
2022-12-07 16:03:50 -0500

    Cleaned up Workbook loaded functions.

--- excel_2_csv_converter.py
+++ excel_2_csv_converter.py
@@ -25,71 +25,66 @@ placed in a directory called "Exports".
 
 from argparse import ArgumentParser
 from csv import writer
-from os import listdir
-from os.path import basename, isdir, join, splitext
+from os.path import isdir, join
 from pathlib import Path
 from typing import Final
-from tqdm import tqdm
 
 from openpyxl import load_workbook, Workbook
 from openpyxl.worksheet.worksheet import Worksheet
+from tqdm import tqdm
 
 WORKBOOK_EXTENSION: Final = '.xlsx'
 EXPORT_ROOT_DIR: Final = 'Exports'
 
 
-def _load_workbook(workbook_path: str) -> dict[str: Workbook]:
+def _load_workbook(workbook_path: Path) -> dict[Path: Workbook]:
     """
-    Loads the Workbook at the given path into memory. If there does not exist a Workbook at the given path, then a
-    FileNoteFoundError is raised. If the Workbook is open in another program, then a PermissionError is raised since
-    only one program can exclusively access an Excel Workbook.
+    Loads the Workbook at the given path. If there does not exist a Workbook at the given path,
+    then a FileNoteFoundError is raised. If the Workbook is open in another program, then a PermissionError is raised
+    since only one program can exclusively access an Excel Workbook. Only the data is loaded from each of the
+    Workbooks so that the values that are returned by formulas are exported instead of the actual formula.
 
     :param workbook_path: a relative or absolute path to a Workbook.
-    :return: a singleton dictionary containing the name of the Workbook as the key and the Workbook as the value.
+    :return: a singleton dictionary containing the path to the Workbook as the key and the loaded Workbook as the value.
     """
     try:
-        return {splitext(basename(workbook_path))[0]: load_workbook(workbook_path, data_only=True)}
+        return {workbook_path: load_workbook(workbook_path, data_only=True)}
     except FileNotFoundError or PermissionError as e:
         problem_word = 'find' if isinstance(e, FileNotFoundError) else 'load'
         print(f'Could not {problem_word} {Workbook.__name__} "{workbook_path}".')
 
 
-def _load_workbooks(dir_path: str) -> dict[str: Workbook]:
+def _load_workbooks(root_dir_path: Path) -> dict[str: Workbook]:
     """
-    Loads all the Workbooks in the given path and returns a dictionary that maps the name of the Workbook to the
-    corresponding loaded Workbook.
+    Recursively scans for all Workbooks that can be found at the given root directory and loads them into a
+    dictionary where the path to the Workbook is mapped to the loaded Workbook.
 
-    :param dir_path: a relative or absolute path to a directory that contains Workbooks.
-    :return: a dictionary containing names of the Workbooks as the keys and the corresponding Workbook as the values.
+    :param root_dir_path: a relative or absolute path to a directory that contains Workbooks.
+    :return: a dictionary containing paths to the Workbooks as the keys and the corresponding Workbooks as the values.
     """
-    dicts = [_load_workbook(join(dir_path, name)) for name in listdir(dir_path) if name.endswith(WORKBOOK_EXTENSION)]
-    workbooks_dict = dict()
-    for singleton in dicts:
-        workbooks_dict |= singleton
-    return workbooks_dict
+    return {path: _load_workbook(path) for path in root_dir_path.rglob('*.' + WORKBOOK_EXTENSION)}
 
 
-def _get_input_source(path: str) -> dict:
+def _get_workbooks_map(input_path: Path) -> dict[Path, Workbook]:
     """
-    Returns either a singleton dictionary if a path 2 a Workbook is given, a dictionary with multiple entries if a
+    Returns either a singleton dictionary if a path to a Workbook is given, a dictionary with multiple entries if a
     directory of Workbooks is given, or an empty dictionary if the given path is not valid or there are no Workbooks in
     the given dictionary.
 
-    :param path: the path to the Workbook or directory of Workbooks that are to be converted.
-    :return: a dictionary containing mappings from name(s) of the Workbook(s) to the corresponding loaded Workbook(s).
+    :param input_path: the path to the Workbook or directory of Workbooks that are to be converted.
+    :return: a dictionary mapping Path(s) to Workbook(s) to the corresponding loaded Workbook(s).
     """
-    if path.endswith(WORKBOOK_EXTENSION):
-        print(f'Found input source "{path}".')
-        return _load_workbook(path)
-    elif isdir(path):
-        print(f'Found input source "{path}".')
-        workbooks_map = _load_workbooks(path)
+    if str(input_path).endswith(WORKBOOK_EXTENSION):
+        print(f'Found input source "{input_path}".')
+        return _load_workbook(input_path)
+    if isdir(input_path):
+        print(f'Found input source "{input_path}".')
+        workbooks_map = _load_workbooks(input_path)
         if not workbooks_map:
-            print(f'No {Workbook.__name__}s found in "{path}".')
+            print(f'No {Workbook.__name__}s found in "{input_path}".')
         return workbooks_map
-    else:
-        print(f'"{path}" is not a valid {Workbook.__name__} or directory.')
-        return dict()
+    print(f'"{input_path}" is not a valid {Workbook.__name__} or directory.')
+    return dict()
 
 
 def _should_write_data(export_path: Path) -> bool:
@@ -174,7 +169,7 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('-path', type=str, required=True, help=f'The name of the {Workbook.__name__} or a path to '
                                                                f'the directory containing {Workbook.__name__}s.')
-    input_source = _get_input_source(parser.parse_args().path)
+    input_source = _get_workbooks_map(parser.parse_args().path)
     if input_source:
         _workbooks2csv(input_source)
 
