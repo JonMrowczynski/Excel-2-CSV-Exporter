e7060d8283ae769755adce12637113052a8f6034
Jon Mrowczynski <jonmrowczynski@gmail.com>
2023-01-08 21:24:34 -0500

    Updated copyright year.

--- excel_2_csv_converter.py
+++ excel_2_csv_converter.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2018-2022 Jon Mrowczynski
+Copyright (c) 2018-2023 Jon Mrowczynski
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
