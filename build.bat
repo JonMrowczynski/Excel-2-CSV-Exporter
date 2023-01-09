d03ede0fbb4538c07d1b417388176143314f6178
Jon Mrowczynski <jonmrowczynski@gmail.com>
2023-01-08 22:32:31 -0500

    Added datetime stamp to executable output.

--- build.bat
+++ build.bat
@@ -1,4 +1,8 @@
 @echo off
 title excel_2_csv_converter build script
 del /Q build, dist
-python -O -m PyInstaller --clean --onefile excel_2_csv_converter.py
+set year=%date:~10,4%
+set month=%date:~4,2%
+set day=%date:~7,2%
+set script_name="excel_2_csv_converter"
+python -O -m PyInstaller --clean --onefile %script_name%.py --name %script_name%_%year%_%month%_%day%
