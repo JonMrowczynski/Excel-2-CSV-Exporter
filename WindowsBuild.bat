649a8132de4cbdc8653fd31c970b012af31785c8
Jon Mrowczynski <jonmrowczynski@gmail.com>
2023-01-30 06:08:51 -0500

    Refactored out script name in build.bat and rebuilt executable.

---
+++ WindowsBuild.bat
@@ -0,0 +1,8 @@
+@echo off
+set script_name="excel_2_csv_converter"
+title %script_name% build script
+del /Q build, dist, *.spec
+set year=%date:~10,4%
+set month=%date:~4,2%
+set day=%date:~7,2%
+python -O -m PyInstaller --clean --onefile %script_name%.py --name %script_name%_%year%-%month%-%day%
