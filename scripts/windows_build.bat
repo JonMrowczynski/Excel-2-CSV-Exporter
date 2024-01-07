97aa0537065e718c9aaf93d7ba75cfb810d7b957
Jon Mrowczynski <jonmrowczynski@gmail.com>
2024-01-07 10:22:44 -0500

    Corrected casing in batch file.

--- scripts/windows_build.bat
+++ scripts/windows_build.bat
@@ -1,9 +1,9 @@
 @echo off
-rmdir /s /q build dist\Windows
+rmdir /s /q build dist\windows
 del *.spec
 set year=%date:~10,4%
 set month=%date:~4,2%
 set day=%date:~7,2%
 :: script_name should match the name of the Python file that should be built. This is most likely the only needed change.
 set script_name="excel_2_csv_converter"
-python -O -m PyInstaller --clean --distpath dist/Windows --onefile %script_name%.py --name %script_name%_%year%-%month%-%day%
+python -O -m PyInstaller --clean --distpath dist/windows --onefile %script_name%.py --name %script_name%_%year%-%month%-%day%
